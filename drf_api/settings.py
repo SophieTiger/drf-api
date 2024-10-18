e866a46995a343313aeeb7ed5ac0b2e57ba545be
Sophie Tigerholm <sophie.tigerholm@gmail.com>
2024-10-18 07:16:59 +0000

    Adjust ALLOWED_HOST and CLIENT_ORIGIN_DEV

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import re
 from pathlib import Path
 import os
 import dj_database_url
@@ -63,7 +64,7 @@ DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
     '8000-sophietiger-drfapi-1gxh1z7lzxy.ws.codeinstitute-ide.net',
-    'codestar100-1ca13721cbe6.herokuapp.com'
+    os.environ.get('ALLOWED_HOST')
     ]
 
 CSRF_TRUSTED_ORIGINS = [
@@ -113,9 +114,10 @@ if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
         os.environ.get('CLIENT_ORIGIN')
     ]
-else:
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
-        r"^https://.*\.gitpod\.io$",
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
 
 CORS_ALLOW_CREDENTIALS = True
